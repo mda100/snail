d775488ceb0326df8904ba09691ff0cd9aca5c2f
mda100 <mdadams10@gmail.com>
2021-12-30 17:41:15 -0800

    working example

--- snail.py
+++ snail.py
@@ -5,6 +5,8 @@ from random import getrandbits
 from typing import final
 from bencodepy import encode, decode
 from bencodepy.exceptions import BencodeDecodeError
+from operator import attrgetter
+from typing import List
 
 ## GLOBALS ##
 
@@ -15,10 +17,37 @@ s = socket(AF_INET, SOCK_DGRAM)
 BOOTSTRAP_NODES = [(gethostbyname("dht.libtorrent.org"), 25401), 
 (gethostbyname("router.bittorrent.com"), 6881), 
 (gethostbyname("dht.transmissionbt.com"), 6881), 
+(gethostbyname("router.utorrent.com"), 6881),
+(gethostbyname("dht.aelitis.com"), 6881)
 ]
 
-routing_table = []
+new_nodes = []
+routing_table = []  # so the "routing table i'm mainting is different then the query table "
 peers_table = []
+
+
+## data ##
+
+def distance(node: bytes, target: bytes = info_hash) -> int:
+    return int.from_bytes(node, 'big')^int.from_bytes(target, 'big')
+
+class Node:
+    def __init__(self, id: bytearray, address: bytearray) -> None:
+        self.id = bytes(id)
+        self.address = (inet_ntoa(bytes(address[:4])), int.from_bytes((address[4:6]),'big'))
+        self.closeness = distance(self.id)
+        self.alive = True
+        pass
+
+    def __eq__(self, other):
+        return self.id==other.id
+    def __hash__(self):
+        return hash(self.id)
+
+    def __repr__(self):
+        return 'id: %s\naddress: %s\ndistance: %s\nalive: %s' % (self.id, self.address, self.closeness, self.alive)
+
+
 
 ### GET PEERS QUERY  ###
 
@@ -33,31 +62,30 @@ get_peers= {
     }
 query = encode(get_peers)
 
+## UTILITY FUNCTIONS ##
 
+# returns updated COPY of routing table
+# removes "dead" nodes, duplicates, sorts by distance, prunes top K closest
+def update_routing_table(routing_table: list = routing_table) -> list:
+    return sorted(list(set([node for node in routing_table if node.alive])), key=lambda node: node.closeness)[:K]
 
-def parse_node_addresses(nodes_info: bytes):
+def parse_nodes(nodes_info: bytes):
     nodes_array = bytearray(nodes_info)
     if (len(nodes_array) % 26 == 0):
-        nodes = [[nodes_array[(26*i):(26*i)+20], nodes_array[(26*i)+20:(26*(i+1))]] for i in range(int(len(nodes_array)/26))]
-        nodes_addresses = list(set([(inet_ntoa(bytes(node[1][:4])), int.from_bytes(bytes(node[1][4:6]),'big')) for node in nodes]))
+        nodes = [Node(id=nodes_array[(26*i):(26*i)+20], address=nodes_array[(26*i)+20:(26*(i+1))]) for i in range(int(len(nodes_array)/26))]
+        return nodes
     else:
         raise Exception('buffer length of nodes is not divisible by 26')
-    return nodes_addresses
 
-
-# pretty good error handling! it keeps runnning but prints exceptions 
 
 def get_peers_utility(r: bytes) -> None:
     try:
         results = decode(r[0])
         if results[b'y'] == b'r':
             if b'nodes' in results[b'r']:
-                new_addresses = parse_node_addresses(results[b'r'][b'nodes'])
-                routing_table.extend(new_addresses)
-            elif b'values' in results[b'r']:
+                new_nodes.extend(parse_nodes(results[b'r'][b'nodes']))
+            if b'values' in results[b'r']: #libtorrent includes both values and peers and keeps traversing until len(peers) == 8
                 peers_table.extend(results[b'r'][b'values']) # may need to parse this
-            else:
-                print('unexpected response content')
         elif results[b'y'] == b'e':
             print(results[b'e'])
         else:
@@ -65,8 +93,20 @@ def get_peers_utility(r: bytes) -> None:
     except BencodeDecodeError as e:
         print(e)
         
+## QUERY FUNCTIONS ## 
+# there's some redundancy here, but i can worry about abstraction later
 
-def get_peers_query(address: tuple, query: bytes = query) -> None:
+def get_peers_query(node: Node, query: bytes = query) -> None:
+    try:
+        s.settimeout(1)
+        s.sendto(query, node.address)
+        r = s.recvfrom(1024)
+        get_peers_utility(r)
+    except timeout as e:
+        node.alive = False
+        print(node.id,e)
+
+def get_peers_query_bootstrap(address: tuple, query: bytes = query) -> None:
     try:
         s.settimeout(1)
         s.sendto(query, address)
@@ -76,104 +116,66 @@ def get_peers_query(address: tuple, query: bytes = query) -> None:
         print(address,e)
 
 
+def one_pass(nodes_list: List[Node] = routing_table) -> None:
+    i = 0 
+    for node in nodes_list:
+        get_peers_query(node)
+        i+=1
 
-def one_pass(address_list: list = routing_table):
+def one_pass_bootstrap(address_list: list = BOOTSTRAP_NODES) -> None:
     for address in address_list:
-        try:
-            get_peers_query(address)
-            break
-        except:
-            pass
+        get_peers_query_bootstrap(address)
 
-## getting a big batch from the bootstrap noodes##
+one_pass_bootstrap()
+routing_table.extend(new_nodes)
+new_nodes.clear()
 
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-one_pass(address_list=BOOTSTRAP_NODES)
-print(routing_table)
-## run on loop until terminate ##
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
-one_pass()
-print(len(routing_table))
+i = 0
+while len(peers_table) < 20:
+    one_pass()
+    routing_table.extend(new_nodes)
+    routing_table = update_routing_table()
+    new_nodes.clear()
+    i+=1
+
 print(peers_table)
+print(i)
 s.close()
 
+## 100 is too many -> smarter !
+## this is a solid working example
+## i should rewrite this in cython
+## refactor it to be simpler and more efficient
+## take care of use cases and errors
+## once this is a tight module ill be gucci 
+## i'm not going to add seeding -> making a mobile p2p file sharer is enoguh work 
 
-## once somebody times out - it gets stuck! im not sure why 
-## there's some weird inconsistencies with the timeouts 
-## but this is enough to get started
-## create a method for managing the routing table
-## create a terminating condition
+##  read through issues on open source bittorrents 
+## a more intelligent way to do this is to "find_node" until K=20 nodes that have no closer distance 
+## no that's not it - there IS a smarter way to terminate this, but rn # limit it! 
+
+# i truly don't know hwy it gets caught in a loop   
+# maybe i slice the nodes to 64 but i only do a pass on the first 8 -> that way if they time out no problem
+
+## it's converging to one number which isn't great 
+## do i have any peers??? -> maybe that is the closest distance ??
+## as soon as i have a terminating condition i have my minimum working example for the crawler
+## at that point i can clean up this file and make it robust 
+## maybe rewrite in cython
+## and port into a new file 
+## work on the handshake until i download 
+## then make this very basic backage very robust 
+## then work on link opening, a web server, a mobile app , etc. 
+
+## yeah it's converging so my first question is -> do peers exist ?? maybe this IS the closest number 
+
+## do the 64 -> 8 thing
+## if it's getting stuck in a loop -> the 8 closest reference eachother or something
+## maybe hve a 
+
+## OH SHIT YEAH BB WE GOT PEERS
+## NOPE IT GOES ON AND ON BC NO TERMINATING CONDIITON WE GOT PEERS
+
+
+### ok yeah so it converges on a number and then is hitting the same ones and then keeps adding peers
+### how to terminate ?
